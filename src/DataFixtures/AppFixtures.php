02cd73c8a7b129fcc1816f07d327df6c150bbd66
Sérgio Barza <sergio.barza@celfocus.com>
2023-08-29 22:29:33 +0100

    Create Factory for Answer and create Fixture for Asnwer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Answer;
 use App\Entity\Question;
+use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,13 +13,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        QuestionFactory::createMany(20);
+        $questions = QuestionFactory::createMany(20);
 
         QuestionFactory::new()
             ->unpublished()
             ->many(5)
             ->create()
         ;
+
+        AnswerFactory::createMany(100, function () use ($questions) {
+            return [
+                'question' => $questions[array_rand($questions)]
+            ];
+        });
 
         $manager->flush();
     }
